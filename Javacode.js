3df02e612c47f9492b6cd385729f70f2b8f0b2b0
Adnane <Adnanessoufi@gmail.com>
2024-05-18 11:41:49 +0100

    getHumanChoice function created
    create a function that asks the user to choose between rock, paper, scissor.

--- Javacode.js
+++ Javacode.js
@@ -5,9 +5,17 @@ function getComputerChoice(){
    //create an array containing rock paper scissors 
     const Main_list = ['rock','paper','scissors'];
    //random choice from that array
-    let choice_index = Math.floor(Math.random()*Main_list.length);
-    choice = Main_list[choice_index]
+    let computer_choice_index = Math.floor(Math.random()*Main_list.length);
+    computer_choice = Main_list[choice_index]
    //return the random choice
-   return choice
+   return computer_choice
 }
+
+//Create a getHumanChoice function that takes the user choice and returns it.
+function getHumanChoice(){
+    //take the user choice by prompting a message and return it
+    let user_choice = prompt("choose between rock,paper,scissor").toLocaleLowerCase().trim()
+    return user_choice
+}
+
 
