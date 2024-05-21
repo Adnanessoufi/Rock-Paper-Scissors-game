c7e806a811bad90750e9791d0accd5e2bba78443
Adnane <Adnanessoufi@gmail.com>
2024-05-21 16:25:11 +0100

    console log was changed to DOM property
    I tried to add the result to the div textcontent

--- Javacode.js
+++ Javacode.js
@@ -25,20 +25,21 @@ let computerScore = 0
 
 //write a 'playRound' function that evaluate the players choices increments the round winner’s score and announce the winner.
 function playRound(playerChoice,computerChoice){
+    const div = document.querySelector('div')
     //when the result will be a tie
     if (playerChoice === computerChoice) {
-        console.log(`It's a tie. You both chose ${playerChoice}`)
+        div.textContent=`It's a tie. You both chose ${playerChoice}`
     // set all cases when it will be a win and increment the score.
     } else if ( (playerChoice === 'rock' && computerChoice ==='scissor') ||
                 (playerChoice === 'scissor' && computerChoice ==='paper') ||
                 (playerChoice === 'paper' && computerChoice ==='rock')){
-        console.log(`You win. ${playerChoice} beats ${computerChoice}`)
+        div.textContent=`You win. ${playerChoice} beats ${computerChoice}`
         playerScore+=1
 
                 }
     // other cases will be definitely a lost.
     else {
-        console.log(`You lost! ${computerChoice} beats ${playerChoice}`)
+        div.textContent=`You lost! ${computerChoice} beats ${playerChoice})`
         computerScore+=1
     }
 }
